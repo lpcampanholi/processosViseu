d29e9827fb5c944a03ebab716fdd0c68f393f672
Luís Plinio <lpcampanholi@gmail.com>
2024-04-30 13:57:07 -0300

    correções aplicadas

--- AtualizarCapa.ps1
+++ AtualizarCapa.ps1
@@ -2,22 +2,22 @@ function CopiarArquivosAntigos {
   param($NomeDoAutor, $NomeDoLivro, $Diretorio)
 
   #Copiar Livro
-  Copy-Item -Path "G:\Drives compartilhados\$diretorio\$nomeDoAutor\$nomeDoLivro\_Arte\_Físico\" -Destination "C:\Users\Viseu\Desktop\$nomeDoLivro" -Recurse
+  Copy-Item -Path "G:\Drives compartilhados\$diretorio\$NomeDoAutor\$NomeDoLivro\_Arte\_Físico\" -Destination "C:\Users\Viseu\Desktop\$NomeDoLivro" -Recurse
 
   #Copiar Gabarito de Capa 14x21
-  Copy-Item -Path "G:\Meu Drive\_Gabaritos Capa\Gabarito_Capa_14x21cm_2023 Folder\Gabarito_Capa_14x21cm.indd" -Destination "C:\Users\Viseu\Desktop\$nomeDoLivro"
+  Copy-Item -Path "G:\Meu Drive\_Gabaritos Capa\Gabarito_Capa_14x21cm_2023 Folder\Gabarito_Capa_14x21cm.indd" -Destination "C:\Users\Viseu\Desktop\$NomeDoLivro"
 
   #Copiar Gabarito de Capa 16x23
-  Copy-Item -Path "G:\Meu Drive\_Gabaritos Capa\Gabarito_Capa_16x23cm_2023 Folder\Gabarito_Capa_16x23cm_2023.indd" -Destination "C:\Users\Viseu\Desktop\$nomeDoLivro"
+  Copy-Item -Path "G:\Meu Drive\_Gabaritos Capa\Gabarito_Capa_16x23cm_2023 Folder\Gabarito_Capa_16x23cm_2023.indd" -Destination "C:\Users\Viseu\Desktop\$NomeDoLivro"
 
   #Abre Pasta do Livro em "Desktop"
-  Invoke-Item -Path "C:\Users\Viseu\Desktop\$nomeDoLivro"
+  Invoke-Item -Path "C:\Users\Viseu\Desktop\$NomeDoLivro"
 
   #Abre Gabarito 14x21
-  Invoke-Item -Path "C:\Users\Viseu\Desktop\$nomeDoLivro\Gabarito_Capa_14x21cm.indd"
+  Invoke-Item -Path "C:\Users\Viseu\Desktop\$NomeDoLivro\Gabarito_Capa_14x21cm.indd"
 
   #Abre os arquivos .ai
-  Get-ChildItem -Path C:\Users\Viseu\Desktop\$nomeDoLivro -Filter "*.ai" -File | ForEach-Object {
+  Get-ChildItem -Path C:\Users\Viseu\Desktop\$NomeDoLivro -Filter "*.ai" -File | ForEach-Object {
     Invoke-Item $_.FullName
   }
 
@@ -29,25 +29,25 @@ function SalvarArquivosAtualizados {
   param($NomeDoAutor, $NomeDoLivro, $Diretorio, $PastaCapaNova)
   
   #Criar pasta "_Descarte" em "_Físico"
-  New-Item -ItemType Directory -Name "_Descarte" -Path "G:\Drives compartilhados\$diretorio\$nomeDoAutor\$nomeDoLivro\_Arte\_Físico"
+  New-Item -ItemType Directory -Name "_Descarte" -Path "G:\Drives compartilhados\$diretorio\$NomeDoAutor\$NomeDoLivro\_Arte\_Físico"
 
   #Mover arquivos para pasta "_Descarte" 
-  Get-ChildItem -Path "G:\Drives compartilhados\$diretorio\$nomeDoAutor\$nomeDoLivro\_Arte\_Físico" | Where-Object { $_.FullName -notlike "*\_Descarte\*" -and $_.Extension -ne ".indd" } | Move-Item -Destination "G:\Drives compartilhados\$diretorio\$nomeDoAutor\$nomeDoLivro\_Arte\_Físico\_Descarte"
+  Get-ChildItem -Path "G:\Drives compartilhados\$diretorio\$NomeDoAutor\$NomeDoLivro\_Arte\_Físico" | Where-Object { $_.FullName -notlike "*\_Descarte\*" -and $_.Extension -ne ".indd" } | Move-Item -Destination "G:\Drives compartilhados\$diretorio\$NomeDoAutor\$NomeDoLivro\_Arte\_Físico\_Descarte"
 
   #Copiar pasta da capa nova para o Drive em "_Físico"
-  Copy-Item -Path "C:\Users\Viseu\Desktop\$nomeDoLivro\$pastaCapaNova" -Destination "G:\Drives compartilhados\$diretorio\$nomeDoAutor\$nomeDoLivro\_Arte\_Físico" -Recurse
+  Copy-Item -Path "C:\Users\Viseu\Desktop\$NomeDoLivro\$pastaCapaNova" -Destination "G:\Drives compartilhados\$diretorio\$NomeDoAutor\$NomeDoLivro\_Arte\_Físico" -Recurse
 
   #Criar pasta "_Descarte" em "_Impressão"
-  New-Item -ItemType Directory -Name "_Descarte" -Path "G:\Drives compartilhados\$diretorio\$nomeDoAutor\$nomeDoLivro\_Impressão"
+  New-Item -ItemType Directory -Name "_Descarte" -Path "G:\Drives compartilhados\$diretorio\$NomeDoAutor\$NomeDoLivro\_Impressão"
 
   #Mover capa antiga para pasta "_Descarte" em "_Impressão"
-  Get-ChildItem -Path "G:\Drives compartilhados\$diretorio\$nomeDoAutor\$nomeDoLivro\_Impressão" -File | Where-Object { $_.Name -match "capa" } | Move-Item -Destination "G:\Drives compartilhados\$diretorio\$nomeDoAutor\$nomeDoLivro\_Impressão\_Descarte"
+  Get-ChildItem -Path "G:\Drives compartilhados\$diretorio\$NomeDoAutor\$NomeDoLivro\_Impressão" -File | Where-Object { $_.Name -match "capa" } | Move-Item -Destination "G:\Drives compartilhados\$diretorio\$NomeDoAutor\$NomeDoLivro\_Impressão\_Descarte"
 
   #Copia nova capa para o Drive em "_Impressão"
-  Get-ChildItem -Path "C:\Users\Viseu\Desktop\$nomeDoLivro\$pastaCapaNova" -Filter "*.pdf" -Recurse | Copy-Item -Destination "G:\Drives compartilhados\$diretorio\$nomeDoAutor\$nomeDoLivro\_Impressão"
+  Get-ChildItem -Path "C:\Users\Viseu\Desktop\$NomeDoLivro\$pastaCapaNova" -Filter "*.pdf" -Recurse | Copy-Item -Destination "G:\Drives compartilhados\$diretorio\$NomeDoAutor\$NomeDoLivro\_Impressão"
 
   #Abre pasta "_Físico" do Drive para Conferir
-  Invoke-Item -Path "G:\Drives compartilhados\$diretorio\$nomeDoAutor\$nomeDoLivro\_Arte\_Físico"
+  Invoke-Item -Path "G:\Drives compartilhados\$diretorio\$NomeDoAutor\$NomeDoLivro\_Arte\_Físico"
   
 }
 
@@ -56,7 +56,7 @@ function SelecionarDiretorio {
   Write-Host "Selecione o caminho para o projeto:"
   Write-Host "1: Projetos finalizados I"
   Write-Host "2: Projetos finalizados II"
-  White-Host "3: Projetos-finalizados III"
+  Write-Host "3: Viseu Criação\Projetos"
 
   $opcao = Read-Host "Digite o número da opção escolhida (1-3)"
   switch ($opcao) {
@@ -72,7 +72,7 @@ function SelecionarDiretorio {
 
 $Diretorio = SelecionarDiretorio
 $NomeDoAutor = Read-Host "Insira o nome do autor"
-$NomeDoLivro - Read-Host "Insira o nome do livro"
+$NomeDoLivro = Read-Host "Insira o nome do livro"
 
 CopiarArquivosAntigos -NomeDoAutor $NomeDoAutor -NomeDoLivro $NomeDoLivro -Diretorio $Diretorio
 
